1e777f146d90420dbedd4e676eec9f36bd1afedf
SoltsMid <carlson.o.hou@gmail.com>
2025-10-02 00:53:51 +0700

    # Added FrontEnd NavBar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,55 @@
+import {Button, Container, Flex, HStack, Text } from '@chakra-ui/react'
+import { Icon } from "@chakra-ui/react"
+import { Link } from 'react-router-dom'
+import React from 'react'
+// import { PlusSquareIcon } from '@chakra-ui/icons'
+import { CiSquarePlus } from "react-icons/ci";
+import { useColorMode} from './ui/color-mode';
+import { IoMoon } from "react-icons/io5"
+import { LuSun } from "react-icons/lu"
+
+const Navbar = () => {
+    const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Container maxW={"95%"} px={4}>
+        <Flex
+        h={16}
+        alignContent={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+            base:"column",
+            sm:"row"
+        }}
+        >
+            <Text
+             fontSize={"40px"}
+             fontWeight={"bold"}
+             textTransform={"uppercase"}
+             textAlign={"center"}
+             bgGradient="to-r" 
+             gradientFrom="cyan.400" 
+             gradientTo="blue.500"
+             bgClip={"text"}
+             >
+                    <Link to={"/"}>Product Store 🛒</Link>
+            </Text>
+
+            <HStack spacing={20} alignItems={"center"}>
+                <Link to={"/create"}>
+                    <Button>
+                        {/* <PlusSquareIcon fontSize={20} /> */}
+                        <CiSquarePlus fontSize={20} />
+                    </Button>
+                </Link>
+
+                <Button onClick={toggleColorMode}>
+                    {/* {colorMode === "light" ? "🌚" : "🌞"} */}
+                    {colorMode === "light" ? <IoMoon /> : <LuSun />}
+                </Button>
+            </HStack>
+        </Flex>
+    </Container>
+  )
+}
+
+export default Navbar
